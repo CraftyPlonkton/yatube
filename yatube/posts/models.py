567e51e93d41e3ac470b35998646d05b74659d02
Barinov Maksim <craftyplonkton@gmail.com>
2022-03-11 15:13:18 +0300

    final view v1.2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -96,10 +96,3 @@ class Follow(models.Model):
         related_name='following',
         verbose_name='Автор'
     )
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['user', 'author'], name='unique_following'
-            )
-        ]
